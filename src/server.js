cc2ba34ba1cc8cb72bf37665bcd33626c11f5f65
Arturo HB <arturo.batistute@gmail.com>
2020-02-03 01:42:41 -0300

    move all the functions to server.js, because of the verification of the send encoding Codenation API

--- src/server.js
+++ src/server.js
@@ -1,16 +1,61 @@
-const movies = require('./index')
 const express = require('express')
 const app = express()
+const fs = require('fs')
+const filePath = ('imdb-movies.json')
+
+let allMovies = (path = filePath) => {
+  let JsonData = fs.readFileSync(path);
+  let movies = JSON.parse(JsonData)
+
+  return movies;
+}
+
+let movies = allMovies();
+
+let randomMovie = () => {
+
+  let data =  movies[Math.floor(Math.random() * movies.length)];
+
+  let results = {
+      "director": data.Director,
+      "movie": data.Title
+  }
+
+  return results;
+}
+
+let directorRandomMovie = (director) => {
+  let data = {};
+
+  for(let key in movies)
+      if(movies.hasOwnProperty(key) && movies[key].Director === director)
+          
+      data[key] = movies[key];
+
+      let resultsToArray = Object.values(data);
+      let onlyOneMovie = (array) => array[Math.floor(Math.random() * array.length)];
+
+      data = onlyOneMovie(resultsToArray)
+      
+      let results = {
+          "director": data.Director,
+          "movie": data.Title
+      }
+      
+  return results;
+}
+
+/* Server */
 
 app.get('/v1/movie', async (req, res, next) => {
 
-  res.status(200).send(movies.randomMovie());
+  res.status(200).send(randomMovie());
 })
 
 app.get('/v1/movie/:director', async (req, res, next) => {
   const { director } = req.params;
   
-  res.status(200).json(movies.directorRandomMovie(director));
+  res.status(200).json(directorRandomMovie(director));
 })
 
 const start = async (port = 8080) => {
